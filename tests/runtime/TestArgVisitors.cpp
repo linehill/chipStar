65aa0aaf2392eb20f544acb32d32c41582b866ae
Henry Linjamäki <henry.mikael.linjamaki@intel.com>
2024-03-14 16:46:22 +0200

    SPIRVFuncInfo: precompute SPVTypeKind::PODByRef arguments

--- tests/runtime/TestArgVisitors.cpp
+++ tests/runtime/TestArgVisitors.cpp
@@ -34,6 +34,12 @@ int main() {
   ArgInfo.emplace_back(
       SPVArgTypeInfo{SPVTypeKind::POD, SPVStorageClass::CrossWorkgroup, 16});
 
+  // Arg 5: Simulate PODByRef - a POD argument too large to fit in
+  // driver's argument buffer so it is instead passed indirectly via a
+  // device buffer.
+  ArgInfo.emplace_back(SPVArgTypeInfo{
+      SPVTypeKind::PODByRef, SPVStorageClass::CrossWorkgroup, 1 << 20});
+
   // Simulate dynamic shared memory pointer. HipDynMem.cpp inserts it
   // at the end of the paremeter list
   ArgInfo.emplace_back(
@@ -42,14 +48,14 @@ int main() {
   SPVFuncInfo FI(ArgInfo);
 
   // Simulate client-side arguments.
-  int a, b, c, d, e;
-  std::vector<void *> ArgListVec{&a, &b, &c, &d, &e};
+  int a, b, c, d, e, f;
+  std::vector<void *> ArgListVec{&a, &b, &c, &d, &e, &f};
   void **ArgList = static_cast<void **>(ArgListVec.data());
 
   // Test visitors.
 
-  assert(FI.getNumClientArgs() == 5);
-  assert(FI.getNumKernelArgs() == 8);
+  assert(FI.getNumClientArgs() == 6);
+  assert(FI.getNumKernelArgs() == 9);
 
   unsigned ArgIdx = 0;
   FI.visitClientArgs([&](const SPVFuncInfo::ClientArg &Arg) {
@@ -75,7 +81,9 @@ int main() {
     else if (Arg.Index == 4)
       assert(Arg.Kind == SPVTypeKind::POD);
     // Skip workgroup pointer for dynamic shared pointer.
-    else {
+    else if (Arg.Index == 5) {
+      assert(Arg.Kind == SPVTypeKind::POD);
+    } else {
       assert(false && "Broken test.");
       exit(1);
     }
@@ -114,6 +122,9 @@ int main() {
       assert(Arg.Kind == SPVTypeKind::POD);
       assert(Arg.Data == ArgListVec.at(4));
     } else if (Arg.Index == 7) {
+      assert(Arg.Kind == SPVTypeKind::PODByRef);
+      assert(Arg.Data == ArgListVec.at(5));
+    } else if (Arg.Index == 8) {
       assert(Arg.Kind == SPVTypeKind::Pointer);
       assert(Arg.isWorkgroupPtr());
       assert(Arg.Data == nullptr);
